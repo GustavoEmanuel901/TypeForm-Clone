bb63211513097320089abfc44eba373adef8706e
Gustavo <gustavoemanuel901@gmail.com>
2020-10-04 16:05:54 -0300

    Animations

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Main, Section, Sticky } from './styles';
+
+import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
+
+import GlobalStyles from './styles/global'
+
+function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
+
+  return (
+    <>
+      <GlobalStyles/>
+
+      <Header/>
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond/>
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className='third'/>
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className='fourth'/>
+        </Section>
+      </Main>
+    </>
+  );
+}
+
+export default App;
